6a836e8fa84f6eb3cb3f2e9145879490a631af03
opeltein <61658879+opeltein@users.noreply.github.com>
2021-03-20 17:32:22 +0100

    zad1ABC

--- Lab 1/build/index.js
+++ Lab 1/build/index.js
@@ -1,3 +1,24 @@
-var header = document.createElement('h1');
-header.innerHTML = "Hello world";
-document.body.appendChild(header);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikacja statystyki</title>
+</head>
+<body>
+    <div class="input-liczby">
+        <input type = "text" id = "liczba1">
+        <input type = "text" id = "liczba2">
+        <input type = "text" id = "liczba3">
+        <input type = "text" id = "liczba4">
+    </div>
+    <div class = "stats-liczby">
+        <input type = "text" id = "suma">
+        <input type = "text" id = "srednia">
+        <input type = "text" id = "min">
+        <input type = "text" id = "max">
+    </div>
+    <script src= "index.js"></script>
+</body>
+</html>
